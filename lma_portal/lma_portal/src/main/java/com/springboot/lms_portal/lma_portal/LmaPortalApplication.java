8aa615fa9b18f249c03c5f65a9d2c08823058525
Jai Khandelwal <jaikhandelwal0987@gmail.com>
2022-06-28 16:43:05 +0530

    start a project

---
+++ lma_portal/lma_portal/src/main/java/com/springboot/lms_portal/lma_portal/LmaPortalApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.lms_portal.lma_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmaPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmaPortalApplication.class, args);
+	}
+
+}
